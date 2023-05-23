bbc03045ac378e34e92eec33092fbb47289f28e3
FedeBatt <59941634+FedeBatt@users.noreply.github.com>
2023-05-23 17:53:43 -0300

    commit

--- deploy.sh
+++ deploy.sh
@@ -24,6 +24,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:FedeBatt/portfolio.git main:gh-pages
+# git push -f git@github.com:FedeBatt/law-web.git main:gh-pages
 
 cd -
